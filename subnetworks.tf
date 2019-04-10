84b8e57e8173c2e85bf3ea245b02d27ccc2d5972
Nick Stone <nick@harelane.com>
2019-04-10 18:05:38 +0100

    Changes for Azure and OCI

---
+++ subnetworks.tf
@@ -0,0 +1,24 @@
+resource "aws_subnet" "subnet1" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-three.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.environment-example-three.id}"
+  availability_zone_id = "euw1-az1"
+}
+
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-three.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-three.id}"
+  availability_zone_id = "euw1-az2"
+}
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment-example-three.id}"
+
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-example-three.cidr_block}"
+    ]
+    from_port = 80
+    protocol = "tcp"
+    to_port = 80
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+// Azure variables
+
+variable azu_subscription_id {}
+variable azu_client_id {}
+variable azu_client_secret {}
+variable azu_tenant_id {}
+
+
+// OCI variables
+
+variable oci_tenancy_ocid {}
+variable oci_user_ocid {}
+variable oci_fingerprint {}
+variable oci_private_key_path {}
+variable oci_region {}
+
+variable "oci_compartment_ocid" {}
